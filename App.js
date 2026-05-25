b91dd9f95b92ca5bdb9c6bd309718083ffb8fa5c
Arthur Spada <arthur.spada@baspan.com.br>
2026-05-25 10:07:46 -0300

    Tela Login

---
+++ App.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useRef } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  Animated,
+  Dimensions,
+  StatusBar,
+} from "react-native";
+import {useNavigation} from '@react-navigation/native';
+import {createStaticNavigation} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Login from "./Login";
+import Home from "./Home";
+import NewAccount from "./NewAccount";
+import EsqueciSenha from "./EsqueciSenha";
+import AutoPart from "./AutoPart";
+
+const RootStack = createNativeStackNavigator({
+  screens: {
+    Home: {
+      screen: Home,
+      options: {
+        headerShown: false
+      }
+    },
+    Login: {
+      screen: Login,
+    },
+    NewAccount: {
+      screen: NewAccount, 
+    },
+    EsqueciSenha: {
+      screen: EsqueciSenha,
+    }
+  },
+});
+
+const Navigation = createStaticNavigation(RootStack);
+
+const { width } = Dimensions.get("window");
+
+export default function App() {return <Navigation/> }
+
+
